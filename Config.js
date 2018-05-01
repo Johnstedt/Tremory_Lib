bac5320240201a0b7422bbca8f651ae7b0cfd911
John-John Markstedt <John-John@Markstedts.org>
2018-05-01 17:37:29 +0200

    updated calls

--- Config.js
+++ Config.js
@@ -1,5 +1,5 @@
 // 'TEST' or 'PRODUCTION'
-const config = 'PRODUCTION' 
+const config = 'TEST' 
 
 export function getServerURL() {
    return config=='TEST' ? 'https://www.tremory.com' : 'https://tremory.com'
@@ -7,4 +7,8 @@ export function getServerURL() {
 
 export function isProduction(){
     return (config == 'PRODUCTION')
+}
+
+export function getStripeKey(){
+    return config == 'TEST' ? 'pk_test_R2NtRUxWFG1Ua0WLVWFZKw5M' : 'pk_live_2IB6LwzQvWq4XYH7KF1P3z4H'
 }
